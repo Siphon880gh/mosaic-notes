037137d0829fd0b1580de903325c3c75dfe8d9db
Weng Fei Fung <weffung@ucdavis.edu>
2022-12-24 20:25:15 -0800

    Friendlier UX; Asseted

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Mosiac
 By Weng Fei Fung.
 
-Create boxes layered in a mosaic way for you to take notes. You can resize these boxes, add rich text, even paste pictures into it. In the end, print the webpage for your note taking purposes at school, work, etc.
+Create boxes layered in a mosaic way for you to print then take notes. You can resize these boxes, add rich text, even paste pictures into it. In the end, print the webpage for your note taking purposes at school, work, etc.
 
 ## Installation
-Run `bower install`. Is a front-end client made in jQuery and Bootstrap.
+Is a front-end client made in jQuery, jQuery UI, and Tailwind CSS. Will work out of the box.
---
+++ assets/index.js
@@ -0,0 +1,64 @@
+
+        window.lastBox = null;
+
+        function toggleClass(querySelected, clssName) {
+            document.querySelector(querySelected).classList.toggle(clssName)
+        }
+
+        function changeGap(querySelected, gap) {
+            console.log("Changed gap")
+            const newGapStyle = `${querySelected} {
+                gap: ${gap}
+            }`
+            $("#dynamic-style-block--gap").remove();
+            $("body").append($(`<style id="dynamic-style-block--gap"></style>`).html(newGapStyle))
+        }
+
+        function precheckCanDuplicateBox() {
+            if (window.lastBox !== null) {
+                const options = [window.lastBox.width() + "px", window.lastBox.height() + "px", window.lastBox.html()]
+                addBox(...options)
+            } else {
+                alert("Error: You never interacted with any boxes. No recent box to duplicate.")
+            }
+        }
+
+        function addBox(width = "100px", height = "100px", html = "&nbsp;") {
+            const $box = $(`<div class="grid-item rounded-sm" contenteditable="true" style="width:${width}; height:${height}">${html}</div>`);
+            $(".grid").append($box);
+        }
+
+        function deleteLastBox() {
+            if (window.lastBox !== null) {
+                let remindMeByText = window.lastBox.text().substr(0, 30);
+                let confirmed = (`Delete the box?\n\n${remindMeByText}`);
+                if (confirmed) {
+                    window.lastBox.remove();
+                }
+            } else {
+                alert("Error: You never interacted with any boxes. No recent box to duplicate.")
+            }
+        }
+
+        function clearCanvas() {
+            if (confirm("Delete all boxes. Are you sure?"))
+                $(".grid-item").remove();
+        }
+
+        $(() => {
+            // Always have resizable grid items
+            setInterval(() => {
+                $(".grid-item:not(.ui-resizable)").resizable()
+            }, 100);
+
+            // Point to last grid item for duplication or deletion
+            $("body").on("click", event => {
+                const gridItem = event.target.matches(".grid-item") ? event.target : event.target.closest(".grid-item");
+                if (gridItem) {
+                    console.group("I'm at: ")
+                    console.log(gridItem);
+                    console.groupEnd();
+                    window.lastBox = $(gridItem);
+                }
+            })
+        });
