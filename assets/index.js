c961506c4860fec2efd712763bff1a1d101fd2f3
Weng Fei Fung <weffung@ucdavis.edu>
2023-01-12 05:25:14 -0800

    Detecting key modifiers pressed down

--- assets/index.js
+++ assets/index.js
@@ -32,7 +32,7 @@
         }
 
         function addBox(width = "130px", height = "100px", html = "&nbsp;") {
-            html = `<span class="handle-sort ui-icon ui-icon-arrow-4-diag"></span>` + html;
+            html = `<span class="handle-rearrange ui-icon ui-icon-arrow-4-diag"></span>` + html;
             const $box = $(`<div class="grid-item rounded-sm unreset" contenteditable="true" style="width:${width}; height:${height}">${html}</div>`);
             $(".grid").append($box);
         }
