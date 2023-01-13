93e3049645969dca453740cf63876f26966dddd2
Weng Fei Fung <weffung@ucdavis.edu>
2023-01-13 02:40:33 -0800

    Saving and loading possible under the hood

--- assets/persist.js
+++ assets/persist.js
@@ -5,19 +5,37 @@
  * Also implement user creating new keys
  * 
  */
-
 function saveBodyHTML(localStorageKey) {
     // Sanitize
     // Warning does not sanitize against Chrome extensions that injected html
     // - Remove classes flagging it was initialized
-    let html = document.body.innerHTML.replaceAll("ui-resizable", "").replaceAll("medium-editor-element", "")
-    let $html = $(html);
+    let html = $(".grid").html();
+    // let $html = $(html);
     // - Remove resize handles so the boxes can be initiated to be resizable and the handles added back
-    $html.find(".ui-resizable-handle").remove();
-    html = $html.html();
+    // debugger;
+
+    html = html.replaceAll("medium-editor-element", "")
+    html = html.replaceAll(`<div class="ui-resizable-handle ui-resizable-e" style="z-index: 90; display: block;"></div>`, "")
+    html = html.replaceAll(`<div class="ui-resizable-handle ui-resizable-s" style="z-index: 90; display: block;"></div>`, "")
+    html = html.replaceAll(`<div class="ui-resizable-handle ui-resizable-se ui-icon ui-icon-gripsmall-diagonal-se" style="z-index: 90; display: block;"></div>`, "")
+    html = html.replaceAll("ui-resizable", "")
+
+    // $html.find(".ui-resizable-handle").remove();
+    // html = $html.html();
+    
     console.log(html)
+    localStorage.setItem(localStorageKey, html)
 }
 
-function loadBodyHTML(html) {
-    document.body.html = html;
+function loadBodyHTML(localStorageKey) {
+    let html = localStorage.getItem(localStorageKey)
+    if(html)
+        $(".grid").html(html)
 }
+
+/**
+ * 
+saveBodyHTML("mosaic__data1")
+loadBodyHTML("mosaic__data1")
+ * 
+ */
